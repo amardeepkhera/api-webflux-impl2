872041ef968a6efee36de585ff1b17b63578f1fb
s301047 <amardeep.singhkhera@iag.com.au>
2020-04-07 16:35:29 +1000

    initial setup

---
+++ src/main/kotlin/au/com/spike/api_webflux_base/Application.kt
@@ -0,0 +1,11 @@
+package au.com.spike.api_webflux_base
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
